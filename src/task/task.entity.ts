c5d0b22cb69254d9b50a8e4c1122e772332a7152
bl00p1ng <msmith@example.com>
2024-01-03 17:01:22 -0500

    Entidad de un Task

---
+++ src/task/task.entity.ts
@@ -0,0 +1,12 @@
+enum TaskStatus {
+    PENDING = 'PENDING',
+    IN_PROGRESS = 'IN_PROGRESS',
+    DONE = 'DONE',
+}
+
+class Task {
+  id: number;
+  title: string;
+  description: string;
+  status: TaskStatus;
+}
