274394ad21c8e7edfa4672e89b888fe947c59454
bl00p1ng <msmith@example.com>
2024-01-03 17:01:12 -0500

    Método GET de prueba

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+    @Get()
+    helloWold(): string {
+        return 'Hello World';
+    }
+}
